78dacc8f702a3e0293559d4b4e09db8c7b0e0c20
Alanmartin7007 <alanmartin7007@gmail.com>
2022-11-18 13:43:47 +0530

    React

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '30e10efe7bmsh9e1005e2ed92ddfp1f96ebjsna2b357a337b1',
+      'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+  };
+  
+  
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "93064e7953b877efe96ec663b3db705f";
